eb00731503b81cbd76d368a9e99005f92eddb877
ThiagoDrakkTech <thiagoabrahao@drakktech.com.br>
2025-06-07 16:12:26 -0300

    adicionado register.php e formulario do client.html

---
+++ register.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Captura os dados do formulário
+    $name = $_POST["nome"];
+    $cpf = $_POST["cpf"];
+    $adress = $_POST["endereco"];
+    $celfone = $_POST["telefone"];
+    $email = $_POST["email"];
+    $password = $_POST["senha"];
+
+    // Define o nome do arquivo CSV
+    $archive = "bd/cadastro.csv";
+
+    // Abre o arquivo para escrita (se não existir, cria)
+    $fp = fopen($arquivo, "a");
+
+    // Escreve os dados no CSV
+    fputcsv($fp, [$name, $cpf, $adress, $celfone, $email, $password]);
+
+    // Fecha o arquivo
+    fclose($fp);
+
+    echo "Cadastro salvo com sucesso!";
+}
+?>
